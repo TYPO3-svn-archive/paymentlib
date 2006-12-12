1a16694736ad9d2401e42321c9495944fabdfc02
pil-dk <pil-dk@735d13b6-9817-0410-8766-e36946ffe9aa>
2006-12-12 14:24:11 +0000

    Add dependencies
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/paymentlib/trunk@4314 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,16 +1,16 @@
 <?php
 
 ########################################################################
-# $Id$
 # Extension Manager/Repository config file for ext: "paymentlib"
-# 
-# Auto generated 24-09-2005 17:08
-# 
+#
+# Auto generated 12-12-2006 15:18
+#
 # Manual updates:
-# Only the data in the array - anything else is removed by next write
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
 ########################################################################
 
-$EM_CONF[$_EXTKEY] = Array (
+$EM_CONF[$_EXTKEY] = array(
 	'title' => 'Payment code library',
 	'description' => 'This code library acts as an abstraction layer for payment methods in general. Have a look at this extension if you need any payment functionality in your TYPO3 project.',
 	'category' => 'misc',
@@ -19,8 +19,6 @@ $EM_CONF[$_EXTKEY] = Array (
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
-	'TYPO3_version' => '3.8.1-0.0.1',
-	'PHP_version' => '5.0.1-0.0.1',
 	'module' => 'mod1',
 	'state' => 'beta',
 	'internal' => 0,
@@ -29,15 +27,24 @@ $EM_CONF[$_EXTKEY] = Array (
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'author' => 'Robert Lemke',
-	'author_email' => 'robert@typo3.org',
-	'author_company' => 'robert lemke medienprojekte',
+	'author' => 'Tonni Aagesen',
+	'author_email' => 't3dev@support.pil.dk',
+	'author_company' => 'pil.dk',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
-	'private' => 0,
-	'download_password' => '',
-	'version' => '0.1.1',	// Don't modify this! Managed automatically during upload to repository.
-	'_md5_values_when_last_written' => 'a:20:{s:32:"class.tx_paymentlib_tceforms.php";s:4:"2dc6";s:21:"ext_conf_template.txt";s:4:"39b7";s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"f085";s:14:"ext_tables.sql";s:4:"e9a5";s:13:"locallang.php";s:4:"497b";s:16:"locallang_db.php";s:4:"ff7b";s:7:"tca.php";s:4:"aa18";s:36:"lib/class.tx_paymentlib_provider.php";s:4:"75b3";s:43:"lib/class.tx_paymentlib_providerfactory.php";s:4:"b25f";s:41:"lib/class.tx_paymentlib_providerproxy.php";s:4:"dbdf";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"44a3";s:14:"mod1/index.php";s:4:"ef10";s:18:"mod1/locallang.php";s:4:"06e8";s:22:"mod1/locallang_mod.php";s:4:"cd73";s:19:"mod1/moduleicon.gif";s:4:"8074";s:48:"tests/tx_paymentlib_providerfactory_testcase.php";s:4:"f70c";s:46:"tests/tx_paymentlib_providerproxy_testcase.php";s:4:"b576";s:49:"tests/fixtures/tx_paymentlib_provider_fixture.php";s:4:"aaef";}',
+	'version' => '0.1.1',
+	'_md5_values_when_last_written' => 'a:20:{s:32:"class.tx_paymentlib_tceforms.php";s:4:"01c1";s:21:"ext_conf_template.txt";s:4:"7494";s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"b1d9";s:14:"ext_tables.sql";s:4:"44f9";s:13:"locallang.php";s:4:"b58a";s:16:"locallang_db.php";s:4:"24c3";s:7:"tca.php";s:4:"d851";s:36:"lib/class.tx_paymentlib_provider.php";s:4:"4dcd";s:43:"lib/class.tx_paymentlib_providerfactory.php";s:4:"f9b8";s:41:"lib/class.tx_paymentlib_providerproxy.php";s:4:"bffe";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"b339";s:14:"mod1/index.php";s:4:"4d2e";s:18:"mod1/locallang.php";s:4:"563c";s:22:"mod1/locallang_mod.php";s:4:"ce2c";s:19:"mod1/moduleicon.gif";s:4:"8074";s:48:"tests/tx_paymentlib_providerfactory_testcase.php";s:4:"b94b";s:46:"tests/tx_paymentlib_providerproxy_testcase.php";s:4:"66d2";s:49:"tests/fixtures/tx_paymentlib_provider_fixture.php";s:4:"76b5";}',
+	'constraints' => array(
+		'depends' => array(
+			'cms' => '',
+			'php' => '5.0.1-',
+			'typo3' => '3.8.1-',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
 );
 
 ?>
