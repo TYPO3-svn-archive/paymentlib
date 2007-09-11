66574ef7039c003e0daf8f9ab0e64bd37496c4c7
pil-dk <pil-dk@735d13b6-9817-0410-8766-e36946ffe9aa>
2007-09-11 14:07:22 +0000

    Released version 0.3.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/paymentlib/trunk@6498 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "paymentlib"
 #
-# Auto generated 18-06-2007 12:24
+# Auto generated 11-09-2007 16:07
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -32,8 +32,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_company' => 'pil.dk',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
-	'version' => '0.2.1',
-	'_md5_values_when_last_written' => 'a:17:{s:9:"Changelog";s:4:"35b6";s:10:"README.txt";s:4:"18e4";s:8:"TODO.txt";s:4:"c95f";s:32:"class.tx_paymentlib_tceforms.php";s:4:"7666";s:21:"ext_conf_template.txt";s:4:"7494";s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"0fea";s:14:"ext_tables.sql";s:4:"36cd";s:13:"locallang.php";s:4:"b58a";s:16:"locallang_db.php";s:4:"24c3";s:7:"tca.php";s:4:"d851";s:36:"lib/class.tx_paymentlib_provider.php";s:4:"09c6";s:43:"lib/class.tx_paymentlib_providerfactory.php";s:4:"f117";s:41:"lib/class.tx_paymentlib_providerproxy.php";s:4:"ee0c";s:48:"tests/tx_paymentlib_providerfactory_testcase.php";s:4:"b94b";s:46:"tests/tx_paymentlib_providerproxy_testcase.php";s:4:"66d2";s:49:"tests/fixtures/tx_paymentlib_provider_fixture.php";s:4:"76b5";}',
+	'version' => '0.3.0',
+	'_md5_values_when_last_written' => 'a:17:{s:9:"Changelog";s:4:"1d6a";s:10:"README.txt";s:4:"18e4";s:8:"TODO.txt";s:4:"deaf";s:32:"class.tx_paymentlib_tceforms.php";s:4:"7666";s:21:"ext_conf_template.txt";s:4:"7494";s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"0fea";s:14:"ext_tables.sql";s:4:"36cd";s:13:"locallang.php";s:4:"b58a";s:16:"locallang_db.php";s:4:"24c3";s:7:"tca.php";s:4:"d851";s:36:"lib/class.tx_paymentlib_provider.php";s:4:"cfd2";s:43:"lib/class.tx_paymentlib_providerfactory.php";s:4:"f117";s:41:"lib/class.tx_paymentlib_providerproxy.php";s:4:"fb2f";s:48:"tests/tx_paymentlib_providerfactory_testcase.php";s:4:"f0b6";s:46:"tests/tx_paymentlib_providerproxy_testcase.php";s:4:"66d2";s:49:"tests/fixtures/tx_paymentlib_provider_fixture.php";s:4:"c6ec";}',
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
